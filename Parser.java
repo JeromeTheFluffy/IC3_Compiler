d6687b54ad30d80dc247d96d2a42b4bcedea2e57
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-22 16:55:32 -0600

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -10,7 +10,8 @@ import java.utils.Scanner;
 import java.lang.system.*;
 import java.io.IOException;
 import java.io.File;
-import java.lang.String
+import java.lang.String;
+import java.lang.Object;
 
 public class Parser extends Scanner throws IOException{
         /*main class of the Parser. */
@@ -29,20 +30,39 @@ public class Parser extends Scanner throws IOException{
         */
         
         //variable declarations
-        String keywords[] = {"showText", "str", };
-        String asmForKeywords[] = {""};
-        public File mainFormFile = new File("out.asm", "w");
+        public File sourceFile = new File(filePath, "r");
+        public File intermediateFile = new File(filePath, "w");
+        public Scanner source = new Scanner(sourceFile);
         /* generate fhe form. for use with GAS from GCC */
+
+        //generate form for intermediate file
+        public int getSizeOfFile(File file;){
+                int sizeOf = 0;
+                while(Parser.source.nextChar() != null){
+                        sizeOf++;
+                }
+                return sizeOf;
+        }
+        // scan text, a universal method
+        public void generateIntermediateFor(String theStringToWrite;){
+                        //generate an intermediate language for the compiler
+                        public String variableName[i];
+                        private int i = 0;
+                        if(theStringToWrite.equals("showText"){
+                                intermediateFile.write("print(%s)\n", variableName);
+                        }
+                        if(theStringToWrite.equals("str")){
+                                variableName[i] = Parser.source.next();
+                                intermediateFile.write("var $%s\n", variableName[i]);
+                                i++
+                        }
+                }
         /*now generate the parser*/
-        public File parse(){
-                String path = main.args[0];
-                File sourceFile = new File(path, "r");
-                File intermediateFile
+        public String getNextTokenInSource(){
+                return Parser.source.next();
         }
-        public static void main(String args[1]){
-                //parse through ice source and return intermediate language
-                parse();
+        public static int main(String args[1]){
         }
-        
-        }
+        return 0;
 }
+

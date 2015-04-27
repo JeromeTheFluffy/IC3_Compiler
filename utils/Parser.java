2853ed6669b22188bd674661109f23e1c4d78c4b
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-27 16:06:36 -0600

    Update Parser.java

--- utils/Parser.java
+++ utils/Parser.java
@@ -1,64 +1,36 @@
 /**this program is covered by the GNU GPL 
  * @author R3xTh33nc0d3r
  * date created: 04/21/2015
- * this program is designed to, very basically, parse through code, look for keywords, and convert it to C.
- * TODO from there, you must assemble it (will change)
+ * this program is nothing more than an extended Scanner
 **/
-package com.icecoldcode
+package IC3_Compiler.utils
+
+native import IC3_compiler.ExtendedFile
 
 import java.utils.Scanner;
 import java.lang.system.*;
 import java.io.IOException;
 import java.io.File;
 import java.lang.String;
-import java.lang.Object;
 
 public class Parser extends Scanner throws IOException{
-        /*main class of the Parser. */
-        /* DOCsTRING:
-        a parser has:
-                a start position
-                a array af keywords
-                a array of the keywords equivilents
-        a Parser can:
-            parse by:
-              looking for keywords
-              looking for logical expressions
-              looking for mathematical expressions
-        generate assembly by
-                taking keywords and writing thier equivilents as well as the main format
-        */
-        
-        //variable declarations
-        public File sourceFile = new File(filePath, "r");
-        public File intermediateFile = new File(filePath, "w");
-        public Scanner source = new Scanner(sourceFile);
-        /* generate fhe form. for use with GAS from GCC */
-        // gets length of a string
-        public int getLength(String stringToUse){
-                int sizeOfString;
-                while(stringToUse != null){
-                        sizeOfString += 1;
-                }
-                return sizeOfString;
+        /*main class of the Parser. can scan chars, ints, strings, mathematical expressions and keywords*/
+        //seek position (very useFul)
+        int seekPos = 0;
+        //constructer:
+        public Parser(String path){
+              //generate new file
+              ExtendedFile *fileToParse = new ExtendedFile
+              File(path, "r");
         }
-
-        //generate form for intermediate file
-        public void generateIntermediateFor(String theString;){
-                        //generate an intermediate language for the compiler
-                        public String variableName[i];
-                        private int i = 0;
-                        if(theString.equals("str"){
-                               String varName = theString.next();
-                               String declaration = 
-                               intermediateFile.write()
-                        }
-                }
-        /*now generate the parser*/
-        public String getNextTokenInSource(){
-                return Parser.source.next();
+        //get int from file
+        public void setSeekPos(int pos){
+                seekPos = pos;
         }
-        public static int main(String args[1]){
+        public int getSeekPos(){
+                return seekPos;
         }
-        return 0;
+        public int readInt(){
+                
+        }
 }
